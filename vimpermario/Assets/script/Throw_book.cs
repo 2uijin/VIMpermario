e23f5e45ad584b6282b1d3810fb55c55c66647e4
2uijin <49094082+2uijin@users.noreply.github.com>
2020-05-18 01:05:50 +0900

    Throw enemy , mainpage

---
+++ vimpermario/Assets/script/Throw_book.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw_book : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public float distance;
+    public LayerMask isLayer;
+    void Start()
+    {
+        Invoke("Destroybook", 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D raycast = Physics2D.Raycast(transform.position, transform.right * -1, distance, isLayer);
+        if (raycast.collider != null) {
+            if (raycast.collider.tag == "plyer")
+                Debug.Log("양아름에게 책으로 맞음ㅋㅋ");
+            Destroybook();
+        }
+        transform.Translate(transform.right * -1f * speed * Time.deltaTime);
+    }
+
+    void Destroybook() {
+        Destroy(gameObject);
+    }
+}
